4800703e748b2f96204f0cf3a0281d17bb3423d0
Julian Darou-Santos <80859386+Jdrty@users.noreply.github.com>
2026-04-12 23:26:40 -0400

    added direction per pixel to render loop

--- rayTracing_SPH4U/include/renderer.h
+++ rayTracing_SPH4U/include/renderer.h
@@ -1,4 +1,9 @@
 #pragma once
 #include <cstdint>
+#include <cmath>
+
+struct Vec3 {
+    float x, y, z;
+};
 
 void render(uint32_t* pixels, int w, int h); // pixels are 4 channels each 8 bit, therefore 32 bit
--- rayTracing_SPH4U/src/renderer.cpp
+++ rayTracing_SPH4U/src/renderer.cpp
@@ -1,9 +1,30 @@
 #include "../include/renderer.h"
 
+Vec3 normalize(Vec3 v) {
+	float len = sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+	return { v.x / len, v.y / len, v.z / len };
+}
+
 void render(uint32_t* pixels, int w, int h) {
-	for (int y = 0; y < h; y++) {
-		for (int x = 0; x < w; x++) {
-			pixels[y * w + x] = 0x00FF0000; // red as example
-		}
-	}
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+
+            float u = (float)x / (float)w;
+            float v = (float)y / (float)h;
+
+            // map to screen space (-1 to 1)
+            float sx = 2.0f * u - 1.0f;
+            float sy = 1.0f - 2.0f * v;
+
+            Vec3 dir = normalize({ sx, sy, -1.0f });
+
+            // direction as color
+            // TEMPORARY!!!!
+            uint8_t r = (dir.x * 0.5f + 0.5f) * 255;
+            uint8_t g = (dir.y * 0.5f + 0.5f) * 255;
+            uint8_t b = (dir.z * 0.5f + 0.5f) * 255;
+
+            pixels[y * w + x] = (r << 16) | (g << 8) | b;
+        }
+    }
 }
